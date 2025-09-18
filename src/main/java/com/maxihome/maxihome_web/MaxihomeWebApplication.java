389fac4891583e10fe0d2ca88eb5482ad6eec8fa
Renzo <renzojcr@hotmail.com>
2025-09-18 05:15:13 -0500

    initial commit - Estructura del proyecto con Spring Boot

---
+++ src/main/java/com/maxihome/maxihome_web/MaxihomeWebApplication.java
@@ -0,0 +1,13 @@
+package com.maxihome.maxihome_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxihomeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxihomeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxihome/maxihome_web/MaxihomeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxihome.maxihome_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxihomeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
